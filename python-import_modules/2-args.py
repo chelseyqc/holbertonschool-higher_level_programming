d177b1d8bba1b79c29b6db0a7258eba7340a6e32
chelseyqc <6721@holbertonstudents.com>
2023-09-25 16:04:46 +0800

    fix: changed print statements

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     if len(sys.argv) == 2:
         print("1 argument:")
         print("1: {}".format(sys.argv[1]))
-    elif len(sys.argv) > 2:
-        print("{} arguments:".format(len(sys.argv)))
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
         for i in range(len(sys.argv)):
             print("{}: {}".format(i, sys.argv[i]))
