41011068f0a4dcca84e170aa83c4a32594f0a662
chelseyqc <6721@holbertonstudents.com>
2023-09-25 16:08:30 +0800

    fix: changed if statement to elif

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,7 +3,7 @@ import sys
 if __name__ == "__main__":
     if len(sys.argv) == 1:
         print("0 arguments.")
-    if len(sys.argv) == 2:
+    elif len(sys.argv) == 2:
         print("1 argument:")
         print("1: {}".format(sys.argv[1]))
     else:
