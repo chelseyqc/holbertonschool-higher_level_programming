29367541f783a470378028f75c6e543ea4122f0b
chelseyqc <6721@holbertonstudents.com>
2023-09-26 08:44:16 +0800

    fix: removed loop

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    for value in range(0, len(str)):
-        if n >= 0:
-            return str[:n] + str[n + 1:]
-        else:
-            return str
+    if n >= 0:
+        return str[:n] + str[n + 1:]
+    else:
+        return str
 
