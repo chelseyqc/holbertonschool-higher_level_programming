3d98765739d0f6989b6737a5b169c014279fa46c
chelseyqc <6721@holbertonstudents.com>
2023-09-26 10:42:49 +0800

    fix: included declaration of a and b within if statement

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 import sys
 from calculator_1 import add, sub, mul, div
-a = int(sys.argv[1])
-b = int(sys.argv[3])
-operator = sys.argv[2]
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>", end=" ")
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    elif operator == '+':
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    operator = sys.argv[2]
+    if operator == '+':
         result = add(a, b)
     elif operator == '-':
         result = sub(a, b)
