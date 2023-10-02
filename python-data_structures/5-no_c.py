a000ee96f0f33ffa4224554fe48caaf27548dfab
chelseyqc <6721@holbertonstudents.com>
2023-10-02 10:05:17 +0800

    fix: account for no string

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    if my_string is None:
+        return None
     for idx in my_string:
         new_string = my_string.translate({ord(idx): None for idx in 'Cc'})
         return new_string
