11e2ef03eb4767c55c00388585e89504ea5a81bd
chelseyqc <6721@holbertonstudents.com>
2023-10-02 10:49:54 +0800

    feat: added a function that adds 2 tuples

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    a = b = c = d = 0
+    if len(tuple_a) >= 1:
+        a = tuple_a[0]
+    if len(tuple_a) >= 2:
+        b = tuple_a[1]
+    if len(tuple_b) >= 1:
+        c = tuple_b[0]
+    if len(tuple_b) >= 2:
+        d = tuple_b[1]
+    return a + c, b + d
