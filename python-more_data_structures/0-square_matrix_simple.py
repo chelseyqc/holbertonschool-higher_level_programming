723faad32a9d43bfa9cd0bf600250c096ae64523
chelseyqc <6721@holbertonstudents.com>
2023-10-03 08:43:22 +0800

    fix: removed comments

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    # nested list comprehension
-    # outer loop: iterates over each row in the input matrix
-    # inner loop: iterates over each element in the current row
     return [[x ** 2 for x in row] for row in matrix]
