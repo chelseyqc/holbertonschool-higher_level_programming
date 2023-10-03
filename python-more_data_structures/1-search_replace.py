e10ef733bf23b101520f653713240ea6c9536895
chelseyqc <6721@holbertonstudents.com>
2023-10-03 09:01:48 +0800

    fix: changed variable names to fit into line limit

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     # compact for loop that iterates over my_list
-    # checks if old_item is equal to search, if yes, it puts in replace
-    # else it puts in old_item in the new_list
-    new_list = [replace if old_item == search else old_item for old_item in my_list]
+    # checks if old is equal to search, if yes, it puts in replace
+    # else it puts in old in the new_list
+    new_list = [replace if old == search else old for old in my_list]
     return new_list
