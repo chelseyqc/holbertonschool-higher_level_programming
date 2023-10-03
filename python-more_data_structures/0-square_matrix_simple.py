52cbebedc26f2f3c7ecde497648ab5e951b58ad1
chelseyqc <6721@holbertonstudents.com>
2023-10-03 08:47:07 +0800

    fix: stored result in variable

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return [[x ** 2 for x in row] for row in matrix]
+    if len(matrix) == 0:
+        return None
+    new_matrix = [[x ** 2 for x in row] for row in matrix]
+    return new_matrix
