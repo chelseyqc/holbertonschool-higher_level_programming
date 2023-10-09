69b41020d2b1e0cd4f40368bffe655972701111d
chelseyqc <6721@holbertonstudents.com>
2023-10-09 16:36:55 +0800

    fix: pycodestyle

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -5,14 +5,14 @@
 class Square:
     """
     Represents a square.
-    
-    Attributes: 
+
+    Attributes:
     __size (int): the size of the square
     """
     def __init__(self, size=0):
         """
         initialises an instance of Square
-        
+
         Args:
         size (int, optional): the size of the square. Defaults to 0
         """
@@ -21,6 +21,7 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
         """
         calculates the area of the square
