68fbe1260b33921d2f4da2df59c7a672e7e483cb
chelseyqc <6721@holbertonstudents.com>
2023-10-09 16:35:28 +0800

    feat: added a class Square that defines a square and returns the current square area

---
+++ python-classes/3-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""a class that defines a square"""
+
+
+class Square:
+    """
+    Represents a square.
+    
+    Attributes: 
+    __size (int): the size of the square
+    """
+    def __init__(self, size=0):
+        """
+        initialises an instance of Square
+        
+        Args:
+        size (int, optional): the size of the square. Defaults to 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        """
+        calculates the area of the square
+
+        Returns:
+            int: the area of the square
+        """
+        return self.__size ** 2
