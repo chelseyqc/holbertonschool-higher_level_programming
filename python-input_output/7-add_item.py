d2f1673d9ba17680e72246223c1a6ac3b398b4d7
chelseyqc <6721@holbertonstudents.com>
2023-10-20 07:07:53 +0800

    fix: changed extend to append

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
 """
-7-add_item module 
+7-add_item module
 """
 import sys
 import os
+
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+my_list = []
 
 filename = "add_item.json"
-if not os.path.exists(filename):
-    my_list = []
-else:
+if os.path.isfile(filename):
     my_list = load_from_json_file(filename)
+args = sys.argv[1:]
 
-my_list.extend(sys.argv[1:])
+for i in args:
+    my_list.append(i)
+
 save_to_json_file(my_list, filename)
