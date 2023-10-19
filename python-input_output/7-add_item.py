076da885ecb025b8968090e2fc1b2d188a786593
chelseyqc <6721@holbertonstudents.com>
2023-10-20 06:42:17 +0800

    fix: moved empty list

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -11,10 +11,9 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
+items = []
 if os.path.isfile(filename) is True:
     items = load_from_json_file(filename)
-else:
-    items = []
 
 items.extend(sys.argv[1:])
 save_to_json_file(items, filename)
