b463634239bcbf27d5eb5052bc86a2752fa8d871
chelseyqc <6721@holbertonstudents.com>
2023-10-20 06:41:21 +0800

    fix: moved import statements

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,10 +2,10 @@
 """
 7-add_item module
 """
-
-
 import sys
 import os
+
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
