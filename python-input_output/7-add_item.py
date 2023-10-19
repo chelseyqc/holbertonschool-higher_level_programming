003cf9c5c9b652f9dda8be34c5dd49c40f456613
chelseyqc <6721@holbertonstudents.com>
2023-10-20 06:54:09 +0800

    fix: pycodestyle

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -11,11 +11,11 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 if not os.path.exists(filename):
-    items = []
+    my_list = []
 else:
-    items = load_from_json_file(filename)
+    my_list = load_from_json_file(filename)
 
 new_item = sys.argv[1:]
 for i in new_item:
-    items.append(i)
-save_to_json_file(items, filename)
+    my_list.append(i)
+save_to_json_file(my_list, filename)
