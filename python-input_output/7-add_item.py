5df4e0c5c431b4e1d201e6375c7573f6fbd1bbf0
chelseyqc <6721@holbertonstudents.com>
2023-10-20 07:26:20 +0800

    fix: added import

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,6 +2,7 @@
 """
 7-add_item module
 """
+import json
 import sys
 import os
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
@@ -15,7 +16,7 @@ if not os.path.exists(filename):
 else:
     my_list = load_from_json_file(filename)
 
-args = sys.argv[1:]
-for i in args:
+new_item = sys.argv[1:]
+for i in new_item:
     my_list.append(i)
 save_to_json_file(my_list, filename)
