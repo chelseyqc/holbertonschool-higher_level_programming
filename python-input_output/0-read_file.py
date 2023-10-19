bb81d31d5357e720e260cc909206ecf0eeb06a75
chelseyqc <6721@holbertonstudents.com>
2023-10-19 10:08:07 +0800

    doc: added docstring

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+"""
+0-read_file module
+"""
+
+
 def read_file(filename=""):
+    """
+    Function that reads a text file (UTF8) and prints it to stdout
+
+    Args:
+        filename (str): the name of the file to be read
+    """
     with open(filename, encoding="utf-8") as f:
         for line in f:
             print(line, end="")
