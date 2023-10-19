5ab54605f21be65d4befb33d038ef3e0e14f853e
chelseyqc <6721@holbertonstudents.com>
2023-10-20 06:37:50 +0800

    fix: changed to if-else

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -11,12 +11,10 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
-try:
+if os.path.isfile(filename):
     items = load_from_json_file(filename)
-except FileNotFoundError:
+else:
     items = []
 
-new_items = sys.argv[1:]
-for i in new_items:
-    items.append(i)
+items.extend(sys.argv[1:])
 save_to_json_file(items, filename)
