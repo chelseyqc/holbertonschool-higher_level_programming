8a4bf9d3e7f816f869cd9c423311b4603a29cd0d
chelseyqc <6721@holbertonstudents.com>
2023-10-20 06:57:25 +0800

    fix: changed to extend

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """
-7-add_item module
+7-add_item module 
 """
 import sys
 import os
-import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
@@ -15,7 +14,5 @@ if not os.path.exists(filename):
 else:
     my_list = load_from_json_file(filename)
 
-new_item = sys.argv[1:]
-for i in new_item:
-    my_list.append(i)
+my_list.extend(sys.argv[1:])
 save_to_json_file(my_list, filename)
