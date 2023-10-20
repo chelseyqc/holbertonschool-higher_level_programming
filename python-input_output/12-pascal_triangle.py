ef24f133ebc8a3b31a775ce68360ffb12f15f61e
chelseyqc <6721@holbertonstudents.com>
2023-10-20 09:32:01 +0800

    feat: added a function for pascals triangle

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""pascal's triangle"""
+
+
+def pascal_triangle(n):
+    """returns a list of integers representing the Pascal's triangle of n"""
+    if n <= 0:
+        return list
+
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        prev_row = triangle[i-1]
+        for row_index in range(1, n):
+            current_row = [1]
+            previous_row = triangle[row_index - 1]
+
+            for element_index in range(1, row_index):
+                current_row.append(previous_row[element_index - 1]
+                                   + previous_row[element_index])
+            current_row.append(1)
+
+            triangle.append(current_row)
+        return triangle
