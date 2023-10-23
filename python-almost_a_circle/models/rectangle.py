89c640b3ef1cfbd16f65af1f2ed579238e1f6b2e
chelseyqc <6721@holbertonstudents.com>
2023-10-23 11:51:18 +0800

    fix: pycodestyle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -12,42 +12,42 @@ class Rectangle(Base):
         self.__height = height
         self.__x = x
         self.__y = y
-    
+
     @property
     def width(self):
         """gets the width attribute"""
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         """sets the width attribute"""
         self.__width = value
-    
+
     @property
     def height(self):
         """gets the height attribute"""
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         """sets the height attribute"""
         self.__height = value
-    
+
     @property
     def x(self):
         """gets the x attribute"""
         return self.__x
-    
+
     @x.setter
     def x(self, value):
         """sets the x attribute"""
         self.__x = value
-    
+
     @property
     def y(self):
         """gets the y attribute"""
         return self.__y
-    
+
     @y.setter
     def y(self, value):
         """sets the y attribute"""
