d9631c7b0331b91e60dff100a4b69c0f9272f4af
chelseyqc <6721@holbertonstudents.com>
2023-10-23 13:26:08 +0800

    fix: changed if statement for width/height

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -23,7 +23,7 @@ class Rectangle(Base):
         """sets the width attribute"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        if value < 0:
+        if value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
 
@@ -37,7 +37,7 @@ class Rectangle(Base):
         """sets the height attribute"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        if value < 0:
+        if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
 
