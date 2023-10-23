c7588c61bc8966bc1c02a5781ebf2cc564b19646
chelseyqc <6721@holbertonstudents.com>
2023-10-24 07:41:04 +0800

    feat: accounted for x and y in def display(self)

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -75,8 +75,10 @@ class Rectangle(Base):
 
     def display(self):
         """prints the Rectangle instance in stdout with #"""
+        if self.y > 0:
+            print("\n" * self.y, end="")
         for _ in range(self.height):
-            print("#" * self.width)
+            print(" " * self.x + "#" * self.width)
 
     def __str__(self):
         """returns str"""
