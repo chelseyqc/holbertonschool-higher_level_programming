0afe8f6fc5859dcac05374038ee96f124b56f039
chelseyqc <6721@holbertonstudents.com>
2023-10-26 08:53:04 +0800

    feat: added to_json_string tests

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -17,3 +17,16 @@ class TestBase(unittest.TestCase):
         """Test - id inputted as arg"""
         b3 = Base(12)
         self.assertEqual(b3.id, 12)
+
+    def test_base_to_json_string(self):
+        """Test - to_json_string list_dictionaries is not empty"""
+        json_dictionary = Base.to_json_string([{'id': 10}])
+        self.assertEqual(json_dictionary, "[{\"id\": 10}]")
+
+    def test_base_to_json_string_empty(self):
+        """Test - to_json_string list_dictionaries is empty"""
+        json_dictionary = Base.to_json_string(None)
+        self.assertEqual(json_dictionary, "[]")
+
+if __name__ == '__main__':
+    unittest.main()
