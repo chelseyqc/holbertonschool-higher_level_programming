08f5c0922482cb5fbcad0ae519416713bf47d6fe
chelseyqc <6721@holbertonstudents.com>
2023-11-23 11:59:38 +0800

    fix: changed order by

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- Lists all shows and all genres linked to that SHOW
-SELECT tv_shows.title as 'title', tv_genres.name AS 'name'
+SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY title, name;
+ORDER BY title ASC, name ASC;
