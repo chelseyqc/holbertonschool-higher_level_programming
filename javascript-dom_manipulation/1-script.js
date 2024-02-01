93c24cbcc6c604818ffc8b2a2209801851d26282
chelseyqc <6721@holbertonstudents.com>
2024-02-01 09:43:12 +0800

    fix: updated to correct header change

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -1,6 +1,6 @@
-let red_header = document.querySelector('header');
+let header = document.querySelector('header');
 red_header.onclick = changeColour;
 
 function changeColour() {
-  red_header.style.color = '#FF0000';
+  header.style.color = '#FF0000';
 }
